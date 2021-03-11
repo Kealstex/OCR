296ed7f9e527e51d20da8a9c66c262def0f954f2
Kealstex <Kealstex@yandex.com>
2021-03-11 13:38:21 +0300

    add brn restart. add method restart for btnRestart.onclick

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,43 @@
+let image = document.getElementById("canvas1");
+let fileinput = document.getElementById("imageOCR");
+let dnd = document.getElementById("drag-and-drop");
+let hint = document.getElementById("hint");
+let btnRestart = document.getElementById("btnRestart");
+let menu = document.getElementById('mobile-menu');
+
 function showMenu() {
-    let menu = document.getElementById('mobile-menu');
     menu.classList.toggle('hidden');
 }
 
 function upload() {
-    var image = document.getElementById("canvas1");
-    var fileinput = document.getElementById("imageOCR");
-    var dnd = document.getElementById("drag-and-drop");
     image.classList.remove("d-none");
     image.src = URL.createObjectURL(fileinput.files[0]);
     dnd.classList.add("d-none");
-    var hint = document.getElementById("hint");
+    hint.style.width = image.offsetWidth + "px";
     hint.classList.remove("d-none");
+    btnRestart.classList.remove("d-none");
     hint.innerHTML = "Files upload!";
 }
 
 function urlUpload() {
-    var url = document.getElementById("start").value;
+    let url = document.getElementById("start").value;
     console.log(url.length);
     if (url.length > 0) {
-        var image = document.getElementById("canvas1");
-        var dnd = document.getElementById("drag-and-drop");
+        image = document.getElementById("canvas1");
+        dnd = document.getElementById("drag-and-drop");
         image.src = url;
         image.classList.remove("d-none");
         dnd.classList.add("d-none");
+        hint.style.width = image.offsetWidth + "px";
+        hint.classList.remove("d-none");
+        hint.innerHTML = "Files upload!";
+        btnRestart.classList.toggle("d-none");
     }
+}
+
+function restart() {
+    image.classList.add("d-none");
+    dnd.classList.remove("d-none");
+    hint.classList.add("d-none");
+    btnRestart.classList.add("d-none");
 }
